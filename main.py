21f2c37ae9f848737f24cc461bc03cf30fe3ab22
jpdicomo <jdicomo@fi.uba.ar>
2021-11-22 18:27:58 -0300

    minifix

--- main.py
+++ main.py
@@ -266,4 +266,4 @@ async def delete_hours(
 
 @app.get("/resources")
 async def get_resources():
-    return json.loads(requests.get("https://anypoint.mulesoft.com/mocking/api/v1/sources/exchange/assets/754f50e8-20d8-4223-bbdc-56d50131d0ae/recursos-psa/1.0.0/m/api/recursos"))
+    return json.loads(requests.get("https://anypoint.mulesoft.com/mocking/api/v1/sources/exchange/assets/754f50e8-20d8-4223-bbdc-56d50131d0ae/recursos-psa/1.0.0/m/api/recursos").text)
