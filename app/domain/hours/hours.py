1403fd3dd01b8f9507455e565e1a0c48e56abb2c
Martin Pata Fraile de Manerola <64995978+mpata2000@users.noreply.github.com>
2021-11-28 13:27:37 -0300

    added task_id and note to hours.py

--- app/domain/hours/hours.py
+++ app/domain/hours/hours.py
@@ -1,4 +1,5 @@
 from typing import List, Optional
+from datetime import date
 
 
 class Hours:
@@ -6,13 +7,17 @@ class Hours:
         self,
         id: str,
         user_id: str,
+        task_id: str,
         day: str,
         minutes: int,
+        note: str,
     ):
         self.id: str = id
         self.user_id: str = user_id
+        self.task_id: str = task_id
         self.day: str = day
         self.minutes: int = minutes
+        self.note: str = note
 
     def __eq__(self, o: object) -> bool:
         if isinstance(o, Hours):
