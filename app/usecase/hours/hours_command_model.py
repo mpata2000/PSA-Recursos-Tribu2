e4922e431c02028f0ae1be0f782ed08daac6f74d
Martin Pata Fraile de Manerola <64995978+mpata2000@users.noreply.github.com>
2021-11-29 16:51:39 -0300

    Added TODO

--- app/usecase/hours/hours_command_model.py
+++ app/usecase/hours/hours_command_model.py
@@ -8,7 +8,7 @@ class HoursCreateModel(BaseModel):
     task_id: str = Field(example="1")
     day: str = Field(example="25/05/2021")
     minutes: int = Field(ge=0, example=150)
-    note: str = Field(example="Descripcion")
+    note: str = Field(example="Descripcion") #TODO: Mejorar documentacion
 
 
 class HoursUpdateModel(BaseModel):
