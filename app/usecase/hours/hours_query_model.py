1260da4910df9d8be211d06e420690aa00a499a8
Martin Pata Fraile de Manerola <64995978+mpata2000@users.noreply.github.com>
2021-11-29 16:52:07 -0300

    Updated ReadModel to show hours id

--- app/usecase/hours/hours_query_model.py
+++ app/usecase/hours/hours_query_model.py
@@ -6,11 +6,12 @@ from app.domain.hours import Hours
 
 
 class HoursReadModel(BaseModel):
+    id: str = Field(example="QK6qXDKUYf3p8x7Vb4SNas")
     user_id: str = Field(example="1")
     task_id: str = Field(example="1")
     day: str = Field(example="25/05/2021")
     minutes: int = Field(ge=0, example=150)
-    note: str = Field(example="Descripcion")
+    note: str = Field(example="Descripcion") #TODO: Mejorar documentacion
 
     class Config:
         orm_mode = True
