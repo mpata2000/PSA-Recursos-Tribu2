6f3347635f1d4edbeff48e81c5ac959d2e9b18b6
Martin Pata Fraile de Manerola <64995978+mpata2000@users.noreply.github.com>
2021-12-04 13:24:47 -0300

    Added tag to resources

--- main.py
+++ main.py
@@ -223,6 +223,7 @@ async def delete_hours(
             "model": ErrorMessageResourcesNotFound,
         },
     },
+    tags=["resources"],
 )
 async def get_resources():
     data = json.loads(requests.get("https://anypoint.mulesoft.com/mocking/api/v1/sources/exchange/assets/754f50e8-20d8-4223-bbdc-56d50131d0ae/recursos-psa/1.0.0/m/api/recursos").text)
