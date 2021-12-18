1a7b7c55c0b7a4098bda2d23c7c0ab86c8b5dd2c
Martin Pata Fraile de Manerola <64995978+mpata2000@users.noreply.github.com>
2021-12-18 01:08:17 -0300

    Small fix

--- app/usecase/hours/hours_command_model.py
+++ app/usecase/hours/hours_command_model.py
@@ -15,8 +15,8 @@ class HoursCreateModel(BaseModel):
 
 
 class HoursPatchModel(BaseModel):
-    user_id: Optional[str]  = Field(example="1")
-    task_id: Optional[str]  = Field(example="1")
+    user_id: Optional[str] = Field(example="1")
+    task_id: Optional[str] = Field(example="1")
     day: Optional[date] = Field(example="2021-05-25")
     hours: Optional[int] = Field(ge=0, le=23, example=3)
     minutes: Optional[int] = Field(ge=0, le=59, example=45)
